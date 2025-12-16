8871c41ef5a59f67deed87bd5b15573b7e2884e3
snowleopard618 <snowleo1342@gmail.com>
2025-12-16 18:20:07 +0900

    add test guide

--- altium_scripts/altium_project_manager.pas
+++ altium_scripts/altium_project_manager.pas
@@ -340,3 +340,188 @@ Begin
     End;
 End;
 
+// Create a new Schematic document and add to current project
+Procedure CreateSchematicDocument;
+Var
+    Workspace   : IWorkspace;
+    Project      : IProject;
+    Doc          : IDocument;
+    DocName      : String;
+    DocPath      : String;
+Begin
+    Try
+        Workspace := GetWorkspace;
+        If Workspace = Nil Then
+        Begin
+            ShowMessage('ERROR: Cannot access workspace');
+            Exit;
+        End;
+        
+        Project := Workspace.DM_FocusedProject;
+        If Project = Nil Then
+        Begin
+            ShowMessage('ERROR: No project is currently open.' + #13#10 + #13#10 +
+                        'Please open or create a project first.');
+            Exit;
+        End;
+        
+        // Get document name from user
+        DocName := InputBox('Create Schematic Document', 
+                           'Enter schematic name (without extension):', 
+                           'Sheet1');
+        If DocName = '' Then Exit;
+        
+        // Create new schematic document
+        Try
+            Doc := Client.CreateDocument('SCH', DocName + '.SchDoc', '');
+            If Doc <> Nil Then
+            Begin
+                // Add to current project
+                Project.DM_AddSourceDocument(Doc.DM_FullPath);
+                
+                // Open the document
+                Client.ShowDocument(Doc);
+                
+                DocPath := Doc.DM_FullPath;
+                ShowMessage('SUCCESS! Schematic document created:' + #13#10 +
+                           'Name: ' + DocName + '.SchDoc' + #13#10 +
+                           'Path: ' + DocPath + #13#10 + #13#10 +
+                           'Document has been added to project and opened.');
+            End
+            Else
+            Begin
+                ShowMessage('ERROR: Could not create schematic document');
+            End;
+        Except
+            ShowMessage('ERROR: Exception creating schematic document');
+        End;
+    Except
+        ShowMessage('ERROR: Could not create schematic document');
+    End;
+End;
+
+// Create a new PCB document and add to current project
+Procedure CreatePCBDocument;
+Var
+    Workspace   : IWorkspace;
+    Project      : IProject;
+    Doc          : IDocument;
+    DocName      : String;
+    DocPath      : String;
+Begin
+    Try
+        Workspace := GetWorkspace;
+        If Workspace = Nil Then
+        Begin
+            ShowMessage('ERROR: Cannot access workspace');
+            Exit;
+        End;
+        
+        Project := Workspace.DM_FocusedProject;
+        If Project = Nil Then
+        Begin
+            ShowMessage('ERROR: No project is currently open.' + #13#10 + #13#10 +
+                        'Please open or create a project first.');
+            Exit;
+        End;
+        
+        // Get document name from user
+        DocName := InputBox('Create PCB Document', 
+                           'Enter PCB name (without extension):', 
+                           'PCB1');
+        If DocName = '' Then Exit;
+        
+        // Create new PCB document
+        Try
+            Doc := Client.CreateDocument('PCB', DocName + '.PcbDoc', '');
+            If Doc <> Nil Then
+            Begin
+                // Add to current project
+                Project.DM_AddSourceDocument(Doc.DM_FullPath);
+                
+                // Open the document
+                Client.ShowDocument(Doc);
+                
+                DocPath := Doc.DM_FullPath;
+                ShowMessage('SUCCESS! PCB document created:' + #13#10 +
+                           'Name: ' + DocName + '.PcbDoc' + #13#10 +
+                           'Path: ' + DocPath + #13#10 + #13#10 +
+                           'Document has been added to project and opened.');
+            End
+            Else
+            Begin
+                ShowMessage('ERROR: Could not create PCB document');
+            End;
+        Except
+            ShowMessage('ERROR: Exception creating PCB document');
+        End;
+    Except
+        ShowMessage('ERROR: Could not create PCB document');
+    End;
+End;
+
+// Create a new PCB Project
+Procedure CreateNewProject;
+Var
+    Workspace   : IWorkspace;
+    Project      : IProject;
+    PrjName      : String;
+    PrjPath      : String;
+    PrjFullPath  : String;
+Begin
+    Try
+        Workspace := GetWorkspace;
+        If Workspace = Nil Then
+        Begin
+            ShowMessage('ERROR: Cannot access workspace');
+            Exit;
+        End;
+        
+        // Get project name from user
+        PrjName := InputBox('Create New Project', 
+                           'Enter project name (without extension):', 
+                           'MyProject');
+        If PrjName = '' Then Exit;
+        
+        // Get project path (optional - will use default if empty)
+        PrjPath := InputBox('Create New Project', 
+                           'Enter project folder path (or leave empty for default):', 
+                           '');
+        
+        // Create project full path
+        If PrjPath = '' Then
+        Begin
+            // Use default workspace location
+            PrjFullPath := PrjName + '.PrjPcb';
+        End
+        Else
+        Begin
+            PrjFullPath := PrjPath + '\' + PrjName + '.PrjPcb';
+        End;
+        
+        // Create new PCB project
+        Try
+            Project := Workspace.DM_CreateProject('PCB', PrjFullPath);
+            If Project <> Nil Then
+            Begin
+                // Open the project
+                Workspace.DM_OpenProject(Project, True);
+                
+                ShowMessage('SUCCESS! New project created:' + #13#10 +
+                           'Name: ' + PrjName + '.PrjPcb' + #13#10 +
+                           'Path: ' + PrjFullPath + #13#10 + #13#10 +
+                           'Project has been created and opened.' + #13#10 +
+                           'You can now add schematic and PCB documents.');
+            End
+            Else
+            Begin
+                ShowMessage('ERROR: Could not create project');
+            End;
+        Except
+            ShowMessage('ERROR: Exception creating project. Make sure the path is valid.');
+        End;
+    Except
+        ShowMessage('ERROR: Could not create project');
+    End;
+End;
+
